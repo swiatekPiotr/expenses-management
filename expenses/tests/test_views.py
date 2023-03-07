6a8f6c059f1fddd1bedce85173340cdf9263177e
swiatek1995@gmail.com <swiatek1995@gmail.com>
2023-03-07 23:39:15 +0100

    view test continuation

--- expenses/tests/test_views.py
+++ expenses/tests/test_views.py
@@ -4,6 +4,11 @@ from ..models import Category, Expense
 
 
 class TestExpenseListView(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        for expense_id in range(8):
+            Expense.objects.create(name='test {expense_id}', amount=8.88)
+
     def set_up(self):
         self.client = Client()
 
@@ -12,7 +17,7 @@ class TestExpenseListView(TestCase):
         self.assertEquals(response.status_code, 200)
 
     def test_url_accessible_by_name(self):
-        response = self.client.get(reverse('expense-list'))
+        response = self.client.get(reverse('expenses:expense-list'))
         self.assertEquals(response.status_code, 200)
 
     def test_correct_template(self):
@@ -23,3 +28,28 @@ class TestExpenseListView(TestCase):
         response = self.client.get('/expenses/expense/list/')
         self.assertTrue('is_paginated' in response.context)
         self.assertTrue(response.context['is_paginated'] == True)
+
+    def test_lists_all_expenses(self):
+        """
+        Get second page and confirm it has remaining 3 items
+        """
+        response = self.client.get(reverse('expenses:expense-list')+'?page=2')
+        self.assertEquals(response.status_code, 200)
+        self.assertEqual(len(response.context['expense_list']), 3)
+
+
+class TestCategoryListView(TestCase):
+    def set_up(self):
+        self.client = Client()
+
+    def test_url_exist(self):
+        response = self.client.get('/expenses/category/list/')
+        self.assertEquals(response.status_code, 200)
+
+    def test_url_accessible_by_name(self):
+        response = self.client.get(reverse('expenses:category-list'))
+        self.assertEquals(response.status_code, 200)
+
+    def test_correct_template(self):
+        response = self.client.get('/expenses/category/list/')
+        self.assertTemplateUsed(response, 'expenses/category_list.html')
